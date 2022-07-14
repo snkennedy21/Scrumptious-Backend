30c866ccd6f0bd9fa984ab127613594d0a427176
Sean Kennedy <snkennedy21@gmail.com>
2022-07-14 15:33:54 -0400

    add multiple models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Recipe
+from .models import Recipe, Measure, FoodItem, Ingredient, Step
 
 admin.site.register(Recipe)
+admin.site.register(Measure)
+admin.site.register(FoodItem)
+admin.site.register(Ingredient)
+admin.site.register(Step)
--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,6 @@
+from enum import unique
 from django.db import models
+from django.forms import CharField
 
 class Recipe(models.Model):
   name = models.CharField(max_length=125)
@@ -11,3 +13,33 @@ class Recipe(models.Model):
   def __str__(self):
     return self.name
   
+class Measure(models.Model):
+  name = models.CharField(max_length=30, unique=True)
+  abbreviation = models.CharField(max_length=10, unique=True)
+
+  def __str__(self):
+    return self.name
+
+
+class FoodItem(models.Model):
+  name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+class Ingredient(models.Model):
+  amount = models.DecimalField(decimal_places=9, max_digits=10)
+  recipe = models.ForeignKey("Recipe", related_name="ingredients", on_delete=models.CASCADE)
+  measure = models.ForeignKey("Measure", on_delete=models.PROTECT)
+  food = models.ForeignKey("FoodItem", on_delete=models.PROTECT)
+
+  def __str__(self):
+    return "Ingredients"
+
+class Step(models.Model):
+  recipe = models.ForeignKey("Recipe", related_name="steps", on_delete=models.CASCADE)
+  order = models.PositiveSmallIntegerField()
+  directions = models.CharField(max_length=300)
+
+  def __str__(self):
+    return f"step {self.order}"
